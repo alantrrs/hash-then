2e51c298418c37024c827a99e3e20eb8971c5b23
Alan <alan.trrs.ngls@gmail.com>
2016-10-11 22:45:09 -0700

    Appveyor (#3)
    * Adds appveyor.yml
    
    * cross-platform testing

--- test/index.js
+++ test/index.js
@@ -4,14 +4,16 @@ var assert = require('assert')
 
 var hash = require('..')
 var fs = require('fs')
+var path = require('path')
+var os = require('os')
 
 var testFile = {
-  path: './test/fixtures/my-file.txt',
+  path: path.join('test', 'fixtures', 'my-file.txt'),
   hash: '8b4781a921e9f1a1cb5aa3063ca8592cac3ee39276d8e8212b336b6e73999798'
 }
 
 var testDir = {
-  path: './test/fixtures',
+  path: path.join('test', 'fixtures'),
   hash: '7b5cba5f5c786dc2f070f3456ac1d34fcdccecb59d328d44e6ece543f302f16c'
 }
 
@@ -29,7 +31,7 @@ describe('hash(path).then(..)', function () {
     }).catch(done)
   })
   it('non-existent paths should return null', function (done) {
-    hash('./test/something_that_doesnt_exist').then(function (hash) {
+    hash(path.join('test', 'something_that_doesnt_exist')).then(function (hash) {
       assert.equal(hash, null)
       done()
     }).catch(done)
@@ -37,10 +39,10 @@ describe('hash(path).then(..)', function () {
 })
 describe('hash(path, output).then(..)', function () {
   it('it should save the tar to ${output}hash.tar', function (done) {
-    const outputDir = '/tmp/'
+    const outputDir = os.tmpdir()
     hash(testDir.path, outputDir).then(function (hash) {
       assert.equal(hash, testDir.hash)
-      assert(fs.lstatSync(outputDir + hash + '.tar').isFile())
+      assert(fs.lstatSync(path.join(outputDir, hash + '.tar')).isFile())
       done()
     }).catch(done)
   })
